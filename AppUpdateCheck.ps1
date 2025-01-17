c8bb91ceea3abff1abf26a688e48392015579be6
LegendaryLuke007 <125861260+LegendaryLuke007@users.noreply.github.com>
2025-01-16 16:04:48 -0900

    Update AppUpdateCheck.ps1
    further edits

--- AppUpdateCheck.ps1
+++ AppUpdateCheck.ps1
@@ -3,26 +3,27 @@
 
 # Show all possible upgrades to the software on the local computer
 winget upgrade --include-unknown
-$gogotag = 'yes'
-$Checkout = ''
+
+$gogotag = "yes"
+$Checkout = ""
 # Prompting User based on information
 
-while (gogotag -eq 'yes') #Loop runs continuously as long as the user isn't finished with upgrading applications.
+while (gogotag -eq "yes") #Loop runs continuously as long as the user isn"t finished with upgrading applications.
   {
   $UpgradeApplications = Read-Host "Do you want to upgrade ALL of your local applications? (yes/no)"
 
-  if ($UpgradeApplications -eq 'yes') #If the user wants to upgrade all applications
+  if ($UpgradeApplications -eq "yes") #If the user wants to upgrade all applications
     {
       wingget upgrade -all  # Update all applications
     
     }
   
-  elseif ($UpgradeApplications -eq 'no')
+  elseif ($UpgradeApplications -eq "no")
     {
   
       $SpecificUpgrade = Read-Host "No Problem! Is there any specific application you would like to upgrade? (yes/no)" #If not all applications, maybe a few specific ones?
 
-      if ($SpecificUpgrade -eq 'yes') #If the User wants to upgrade a specific Application
+      if ($SpecificUpgrade -eq "yes") #If the User wants to upgrade a specific Application
         {
       
           winget upgrade --include-unknown #Shows full List of applications that are out of date
@@ -33,7 +34,7 @@ while (gogotag -eq 'yes') #Loop runs continuously as long as the user isn't fini
             {
               $DoubleConfirmation = Read-Host "Are you sure you want to upgrade?"  #Double check that they want to upgrade
             
-            if ($DoubleConfirmation = -eq 'yes') 
+            if ($DoubleConfirmation = -eq "yes") 
               {
               
                 winget upgrade --id $ApplicationName #Application is upgraded
@@ -44,14 +45,14 @@ while (gogotag -eq 'yes') #Loop runs continuously as long as the user isn't fini
         }
     }
     
-    if ($Checkout -eq '') #If the user doesn't upgrade anything then it should come here
+    if ($Checkout -eq "") #If the user doesn"t upgrade anything then it should come here
       {
       $Checkout = Read-Host "OK!, $ApplicationName Should be upgraded, do you need to upgrade something else?"
       }
       
     if ($Checkout -eq "no")
     {
-    gogotag = 'no' #Turns off while loop and stops the program from running any more times
+    gogotag = "no" #Turns off while loop and stops the program from running any more times
     }
   }
   Read-Host "Great! Have a wonderful Day :)!"
